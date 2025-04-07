025f22d23e58f60d728ddb8bc05d33559e31f846
Pedro Marinho R. C. Corrêa <pedro_marinho_17@hotmail.com>
2025-04-07 18:27:56 -0300

    Adicionando Comentario em PesoIdeal

--- src/componentes/PesoIdeal.jsx
+++ src/componentes/PesoIdeal.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
-const PesoIdeal = ({ altura }) => {
-  const alturaMetros = parseFloat(altura) / 100;
+const PesoIdeal = ({ altura }) => { // inicalização com arrow function
+  const alturaMetros = parseFloat(altura) / 100 ; // transforma a altura de CM para Metros e atribuí o valor para a variavel alturaMetros
 
-  if (!alturaMetros || alturaMetros <= 0) {
+  if (!alturaMetros || alturaMetros <= 0) { // caso  alturaMetros tiver valor falso ou for <= 0 então retornará a mensagem
     return <Text style={styles.error}>Altura inválida para calcular o peso ideal.</Text>;
   }
-
+  // cálculo e atibuição para as variáveis dos pesos ideias
   const pesoIdealMin = (alturaMetros * alturaMetros * 18.5).toFixed(2);
-  const pesoIdealMax = (alturaMetros * alturaMetros * 24.9).toFixed(2);
-
-  return (
+  const pesoIdealMax = (alturaMetros * alturaMetros * 24.9).toFixed(2); 
+  return ( // retorna os valores das variáveis de pesos ideais 
     <Text style={styles.pesoIdeal}>
       Peso Ideal Mínimo: {pesoIdealMin} kg{'\n'}
       Peso Ideal Máximo: {pesoIdealMax} kg
